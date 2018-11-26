79dcf84b10a1cbaed8409b15db0648d0db6936ea
alireza_inspiron <alireza.barkhordari@gmail.com>
2018-11-26 00:51:16 -0800

    layout updated

--- website/src/components/Layout/NavLink/NavLink.js
+++ website/src/components/Layout/NavLink/NavLink.js
@@ -11,9 +11,10 @@ class NavLink extends React.Component {
     return(
       <div className={styles.container}>
         <Link to={this.props.to}>
-          <h5 className={styles.link}>
+          <img className={styles.icon} src={this.props.icon} alt={this.props.name}/>
+          <span className={styles.link}>
             {this.props.name}
-          </h5>
+          </span>
         </Link>
       </div>
     )
